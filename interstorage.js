c807a01985bcf290e2b8eb12fb0f4e4226d34eac
m93a <m93a.cz@gmail.com>
2015-02-23 10:43:11 +0100

    rename

---
+++ interstorage.js
@@ -0,0 +1,45 @@
+
+   (function(){
+     
+     // Technologies used:
+     //  * local storage or cookies
+     //  * cross-origin communication
+     //  * promises
+     
+     var frame = document.createElement("iframe");
+     frame.src = "https://raw.githubusercontent.com/interstorage/interstorage/master/storage.html";
+     frame.style.display = "none";
+     document.body.appendChild(frame);
+     Object.freeze(frame);
+     
+     var pending = [];
+     
+     window.globalStorage = Object.create(null);
+     
+     globalStorage.get = function(name){
+       frame.contentWindow.postMessage([
+        "get", pending.length, name
+       ],"*");
+       return new Promise(function(r){
+         pending.push(r);
+       });
+     };
+     
+     globalStorage.set = function(name,value){
+       frame.contentWindow.postMessage([
+        "set", name, value
+       ],"*");
+     };
+     
+     globalStorage.remove = function(name){
+       frame.contentWindow.postMessage([
+        "rm", name
+       ],"*");
+     };
+     
+     addEventListener("message",function(e){
+       if(e.source !== frame.contentWindow){ return; }
+       pending[ e.data[0] ]( e.data[1] );
+     });
+     
+   })();
